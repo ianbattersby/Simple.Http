94e5245396b5bac4ed4fc5618377d5184749dbac
ianbattersby <ian.battersby@gmail.com>
2014-02-05 23:14:43 +0000

    Add test for faulted state return.

---
+++ src/Simple.Http.Tests.Unit/ApplicationTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Simple.Http.Tests.Unit
+{
+    using System.IO;
+    using System.Threading.Tasks;
+
+    using Simple.Http.OwinSupport;
+
+    using Xunit;
+
+    public class ApplicationTests
+    {
+        [Fact]
+        public void HandlerExceptionReturnsFaultedTask()
+        {
+            var stream = new MemoryStream();
+            var context = new OwinContext(new Dictionary<string, object>
+                                                    {
+                                                        { "host.AppName", "TestApp" },
+                                                        { "server.RemoteIpAddress", "1.2.3.4" },
+                                                        { "owin.RequestProtocol", "HTTP" },
+                                                        { "owin.RequestMethod", "GET" },
+                                                        { "owin.RequestBody", (Stream)stream },
+                                                        { "owin.RequestPath", "/some/exception" },
+                                                        { "owin.RequestQueryString", string.Empty },
+                                                        { "owin.RequestHeaders", new Dictionary<string, string[]>
+                                                                                     {
+                                                                                         { "X-Something", new [] { "somevalue" } }
+                                                                                     }}});
+            var task = Application.Run(context);
+
+            task.ContinueWith(
+                t =>
+                    {
+                        Assert.IsAssignableFrom<Task>(task);
+                        Assert.True(task.IsFaulted);
+                        Assert.False(task.IsCanceled);
+                        Assert.NotNull(task.Exception);
+                        Assert.Equal("This should result in a faulted Task exception.", task.Exception.Message);
+                    },
+                    TaskContinuationOptions.OnlyOnRanToCompletion);
+        }
+    }
+
+    [UriTemplate("/some/exception")]
+    public class ExceptionEndpoint : IGet
+    {
+        public Status Get()
+        {
+            throw new Exception("This should result in a faulted Task exception.");
+        }
+    }
+}
